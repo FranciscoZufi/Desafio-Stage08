5499bc76d4fe25ebe3205fdbecd5367ec9a374b9
Francisco <francar76@hotmail.com>
2023-01-10 09:22:28 -0300

    save

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,13 @@
+const knex = require('../database/knex')
+
+class TagsController {
+  async index(request, response) {
+    const { note_id, id, user_id } = request.params
+
+    const tags = await knex('tags').where({ note_id, id, user_id })
+
+    return response.json(tags)
+  }
+}
+
+module.exports = TagsController
---
+++ src/controllers/UsersController.js
@@ -0,0 +1,76 @@
+const knex = require('../database/knex')
+const { hash, compare } = require('bcryptjs')
+const AppError = require('../utils/AppError')
+
+class UsersController {
+  async create(request, response) {
+    const { name, email, password } = request.body
+
+    const database = await knex()
+    const checkUserExist = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+
+    if (checkUserExist) {
+      throw new AppError('this email is already in use')
+    }
+    const hashedPassword = await hash(password, 8)
+    await database.run(
+      'INSERT INTO users(name, email, password) VALUES (?, ?, ?)',
+      [name, email, hashedPassword]
+    )
+
+    return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const database = await knex()
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    if (!user) {
+      throw new AppError('User not found')
+    }
+
+    const userWithUpdatedEmail = await database.get(
+      'SELECT * FROM users WHERE email = (?)',
+      [email]
+    )
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError('this email is already in use')
+    }
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError(
+        'you need to enter the old password to set the new password!'
+      )
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+      if (!checkOldPassword) {
+        throw new AppError('password does not match')
+      }
+      user.password = await hash(password, 8)
+    }
+
+    await database.run(
+      `
+    UPDATE users SET 
+      name = ?, 
+      email = ?, 
+      password = ?,
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    )
+    return response.json()
+  }
+}
+
+module.exports = UsersController
