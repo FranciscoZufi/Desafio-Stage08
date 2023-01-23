89ed06abca34c06877957d23f0f400e3100e4855
Francisco <francar76@hotmail.com>
2023-01-23 09:09:30 -0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
 require('express-async-errors')
 
+const migrationsRun = require('./database/sqlite/migrations')
 const AppError = require('./utils/AppError.js')
 const { response } = require('express')
 const express = require('express')
+const uploadConfig = require('./configs/upload')
+
+const cors = require('cors')
+
 const routes = require('./routes')
+migrationsRun()
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 
 app.use((error, request, response, next) => {
-  console.log({ error })
   if (error instanceof AppError) {
     return response.status(error.statusCode).json({
       status: 'error',
